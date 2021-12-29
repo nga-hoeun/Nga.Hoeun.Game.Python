21328ce157be49dc36987d1c39d3e24951697fa7
Student <student@PNCA12-08.pnc.lan>
2021-12-29 08:24:32 +0700

    Final touch

--- game.py
+++ game.py
@@ -1,6 +1,8 @@
 # Import modules
 from tkinter import *
 import random
+import time
+from tkinter import font
 
 
 
@@ -15,11 +17,17 @@ root.geometry("960x540")
 # Add image file
 bg=PhotoImage(file="image/background.gif")
 run=PhotoImage(file="image/run.png")
-enemy=PhotoImage(file="image/soldier.png")
 topEnemy=PhotoImage(file="image/helicopter.png")
-playerBullet=PhotoImage(file="image/bulletPlayer.png")
+playerBullet=PhotoImage(file="image/playerbullet.png")
+playerBulletTop=PhotoImage(file="image/image.png")
 shooter=PhotoImage(file="image/shoot.png")
-
+topEnemyBullet=PhotoImage(file="image/bomb.png")
+explosion=PhotoImage(file="image/explosion.png")
+life=PhotoImage(file="image/life (2).png")
+dead=PhotoImage(file="image/die.png")
+win=PhotoImage(file="image/win (2).png")
+winText=PhotoImage(file="image/youWin.png")
+lost=PhotoImage(file="image/gameOver.gif")
 # Create frame
 frame=Frame()
 frame.master.title("Nga Hoeun Game Project")
@@ -35,17 +43,16 @@ background=canvas.create_image(0,0,image=bg,anchor="nw")
 #---------------------------------------------------------------------------------
 # Constants
 #---------------------------------------------------------------------------------
+POINTS=0
+POINTS_TO_WIN=10
 
-ENEMY1_X_START_POSITION = 850
-ENEMY1_Y_START_POSITION = 300
 
 ENEMY2_X_START_POSITION = 0
 ENEMY2_X_END_POSITION = 960
 ENEMY2_Y_START_POSITION = 0
 ENEMY2_Y_END_POSITION = 40
 
-ENEMY2_INCREMENT= 20
-ENEMY1_INCREMENT=-20
+ENEMY2_INCREMENT=10
 
 PLAYER_Y_START_POSITION = 350
 PLAYER_X_START_POSITION = 120
@@ -54,7 +61,13 @@ PLAYER_Y_POSITION_INCREMENT = 120
 
 PLAYER_BULLET_POSITION=340
 PLAYER_BULLET_INCREMENT=20
+ENEMY2_BULLET_X_POSITION=50
+ENEMY2_BULLET_Y_POSITION=70
 
+
+RANDOM_ENEMY_TIME1=random.randrange(2000,4000)
+
+POINTS=0
 #---------------------------------------------------------------------------------
 # Variables
 #---------------------------------------------------------------------------------
@@ -63,44 +76,125 @@ player_y = PLAYER_Y_START_POSITION
 player=canvas.create_image(player_x, player_y, image=run,anchor="center")
 
 # enemies
-enemy1_x=ENEMY1_X_START_POSITION
-enemies=canvas.create_image(enemy1_x, ENEMY1_Y_START_POSITION, image=enemy,anchor="nw",tags="gone")
-enemy2_y=ENEMY2_Y_START_POSITION
 enemy2_X_End=ENEMY2_X_END_POSITION
+enemy2_y=ENEMY2_Y_START_POSITION
 enemy2_X_Start=ENEMY2_X_START_POSITION
-enemies2=canvas.create_image(ENEMY2_X_START_POSITION,enemy2_y,image=topEnemy,anchor="nw",tags="delete")
+arrayOfEnemiesUp=[]
+arrayOfEnemiesRight=[]
+# enemies2=canvas.create_image(ENEMY2_X_START_POSITION,enemy2_y,image=topEnemy,anchor="nw",tags="delete")
+# Points added
 
-# Bullets
+# Bullets Player
+arrayOfBulletsEnemy=[]
+arrayOfBulletsUp=[]
 playerPosition=canvas.coords(player)
 bullet_move=PLAYER_BULLET_INCREMENT
+pointsDisplay=POINTS
 
+
+# Bullets Enemy2
+startBullet2X=ENEMY2_BULLET_X_POSITION
+startBullet2Y=ENEMY2_BULLET_Y_POSITION
+# Scores
+text=canvas.create_text(525,23,text=pointsDisplay,font=("Purisa",26),fill="red")
+# Lives
+live=canvas.create_text(40,20,text="LIVES:",fill="red",font=("Purisa",18))
+live1=canvas.create_image(80,5,image=life,anchor="nw",tags="lifeOne")
+live2=canvas.create_image(120,5,image=life,anchor="nw",tags="lifeTwo")
+live3=canvas.create_image(160,5,image=life,anchor="nw",tags="lifeThree")
+scores=canvas.create_text(450,20,text="SCORES:",fill="red",font=("Purisa",18))
+die=0
 #---------------------------------------------------------------------------------
 # functions
 #---------------------------------------------------------------------------------
-# Bullets for players
+# Load the game
+def gameLoading3():
+    canvas.create_text(450,300,text="3",fill="red",font=("Purisa",38),tags="three")
+def gameLoading2():
+    canvas.create_text(450,300,text="2",fill="red",font=("Purisa",38),tags="two")
+def gameLoading1():
+    canvas.create_text(450,300,text="1",fill="red",font=("Purisa",38),tags="one")
+# Time to win
+def winGame():
+    global pointsDisplay,die
+    if pointsDisplay==POINTS_TO_WIN and die<=3:
+        canvas.delete(all)
+        canvas.create_image(0,0,image=bg,anchor="nw")
+        canvas.create_image(400,300,image=win)
+        canvas.create_image(400,120,image=winText)
+# Move each individual bombs from top enemies
+def bulletEnemies(arrayOfBulletsEenemy):
+    global positionEnemies,another1,anotherBullet
+    for bullets in arrayOfBulletsEenemy:
+        canvas.move(bullets,0,ENEMY2_INCREMENT)
+# Create enemy one at a time when an enemy is dead
+def addEnemies2():
+    global enemies2,positionEnemies,topBullet,arrayOfBulletsEnemy
+    positionEnemies=random.randrange(20,850)
+    enemies2=canvas.create_image(positionEnemies,80,image=topEnemy,tags="topEn")
+    topBullet=canvas.create_image(positionEnemies,80,image=topEnemyBullet,anchor="nw",tags="top")
+    arrayOfBulletsEnemy.append(topBullet)
+    
+
+# Shoot bullets at enemies when right-click on the mouse
 def shootEnemies(event):
-    global playerPosition
-    playerShot=canvas.create_image(playerPosition[0],PLAYER_BULLET_POSITION,image=playerBullet,tags="shot")
-    playerBulletMovement()
+    global playerPosition,topShot,arrayOfBulletsUp
+    topShot=canvas.create_image(playerPosition[0],PLAYER_BULLET_POSITION,image=playerBulletTop,tags="up")
+    arrayOfBulletsUp.append(topShot)
+    
+# Remove
 
-
-def playerBulletMovement():
-    global playerPosition
-    global bullet_move
-    bullet_X=playerPosition
-    if bullet_X[0]<ENEMY2_X_END_POSITION:
-        # canvas.move(,bullet_X[0],0)
-        canvas.after(20,playerBulletMovement)
-    else:
-        canvas.delete("shot")
-
-
-
+# Move the player bullet while checking to kill and add points to the scoreboard
+def moveBullet():
+    global enemies2,topShot,pointsDisplay,arrayOfBulletsUp,topBullet,arrayOfBulletsEnemy
+    bulletEnemies(arrayOfBulletsEnemy)
+    for bullets in arrayOfBulletsUp:
+        winGame()
+        canvas.move(bullets,0,-20)
+        if len(canvas.coords(bullets))>0:
+            if canvas.coords(bullets)[0]>=canvas.coords(enemies2)[0]-40 and canvas.coords(bullets)[0]<=canvas.coords(enemies2)[0]+40 and canvas.coords(bullets)[1]>=canvas.coords(enemies2)[1]-20 and canvas.coords(bullets)[1]<=canvas.coords(enemies2)[1]+20:
+                pointsDisplay+=1
+                canvas.delete(bullets)
+                canvas.delete(enemies2)
+                arrayOfBulletsUp.remove(bullets)
+                addEnemies2()
+                displayScore(pointsDisplay)   
+        deadPlay()
+    canvas.after(50,moveBullet)
+# Change the score when it is added
+def displayScore(pointsDisplay):
+    canvas.itemconfig(text,text=pointsDisplay)
+# Lost
+def lostGame():
+    canvas.delete(all)
+    canvas.create_image(450,300,image=dead)
+    canvas.create_image(450,120,image=lost)
+# Check for bombs of enemies hitting the player and lost lives
+def deadPlay():
+    global player,arrayOfBulletsEnemy,die
+    posPlayer=canvas.coords(player)
+    for bullets in arrayOfBulletsEnemy:
+        topBulletPos=canvas.coords(bullets)
+        if len(topBulletPos)>0:
+            if (posPlayer[0]>=topBulletPos[0]-20 and posPlayer[0]<=topBulletPos[0]+20 and posPlayer[1]>=topBulletPos[1]-20 and posPlayer[1]<=topBulletPos[1]+20):
+                canvas.create_image(posPlayer[0],posPlayer[1]-20,image=dead,anchor="nw",tags="die")
+                die=+1
+                canvas.delete(player)
+                canvas.delete(bullets)
+                canvas.delete(enemies2)
+                canvas.delete(topShot)
+                if die==1:
+                    canvas.delete("lifeThree")
+                elif die==2:
+                    canvas.delete("lifeTwo")
+                elif die==3:
+                    canvas.delete("lifeOne")
+                else:
+                    lostGame()                
 # move left or right
 # @param moveRight : if true move right if false move left
 def moveHorizontaly(moveRight) :
-    global player_x
-    global playerPosition
+    global player_x,playerPosition
     if moveRight:
         new_player_x = player_x + PLAYER_X_POSITION_INCREMENT
     else:
@@ -118,72 +212,24 @@ def movePlayerRight(event):
 def movePlayerLeft(event):
     moveHorizontaly(False)
 
-
-def jumpPlayer(event) :
-    go_up()
-
-def go_up():
-    global player_y
-
-    new_player_y = player_y - 1
-     
-    if new_player_y > PLAYER_Y_START_POSITION - PLAYER_Y_POSITION_INCREMENT:
-        player_y = new_player_y
-        canvas.itemconfig(player, canvas.coords(player,player_x ,player_y))
-        canvas.after(1,go_up)
-
-    else :
-        go_down()
-
-
-def go_down():
-    global player_y
-
-    new_player_y = player_y + 1
-     
-    if new_player_y < PLAYER_Y_START_POSITION:
-        player_y = new_player_y
-        canvas.itemconfig(player, canvas.coords(player,player_x ,player_y))
-        canvas.after(1,go_down)
-
-
-
-def movingEnemies():
-    global enemy1_x
-    if enemy1_x>0:
-        canvas.move(enemies,ENEMY1_INCREMENT,0)
-        canvas.after(100,movingEnemies)
-        enemy1_x+=ENEMY1_INCREMENT
-    else:
-        canvas.delete("gone")
-movingEnemies()
-    
-
-def movingAirplane():
-    global enemy2_y,enemy2_X_End,enemy2_X_Start
-    if enemy2_X_Start < enemy2_X_End-100:
-        if enemy2_y<ENEMY2_Y_END_POSITION:
-            move_y_down=random.randrange(0,40)
-            canvas.move(enemies2,ENEMY2_INCREMENT,move_y_down)
-            enemy2_y+=move_y_down
-            canvas.after(100,movingAirplane)
-        elif enemy2_y>=ENEMY2_Y_START_POSITION:
-            move_y_up=random.randrange(-40,0)
-            canvas.move(enemies2,ENEMY2_INCREMENT,move_y_up)
-            enemy2_y+=move_y_up
-            canvas.after(100,movingAirplane)
-        enemy2_X_Start+=ENEMY2_INCREMENT
-    else:
-        canvas.delete("delete")
-movingAirplane()
-# Shooting
-# def shootTime(event):
-#     positionPlayer=canvas.coords(player)
-#     playerShot=canvas.create_image(positionPlayer[0],340,image=playerBullet,tags="shot")
-#     print(positionPlayer)
-
-
-
+def deleteLoading3():
+    canvas.delete("three")
+def deleteLoading2():
+    canvas.delete("two")
+def deleteLoading1():
+    canvas.delete("one")
+# Start the game
+def startGame():
+    canvas.delete(all)
+    canvas.after(1000,gameLoading3)
+    canvas.after(2000,deleteLoading3)
+    canvas.after(2000,gameLoading2)
+    canvas.after(3000,deleteLoading2)
+    canvas.after(3000,gameLoading1)
+    canvas.after(4000,deleteLoading1)
+    moveBullet()
+    addEnemies2()
+startGame()
 
 #---------------------------------------------------------------------------------
 # bind events
@@ -191,8 +237,7 @@ movingAirplane()
 
 root.bind("<Left>",movePlayerLeft)
 root.bind("<Right>",movePlayerRight)
-root.bind("<Up>",jumpPlayer)
-root.bind("<w>",shootEnemies)
+canvas.bind("<Button-1>",shootEnemies)
 
 canvas.pack(fill="both",expand=True)
 root.mainloop()
